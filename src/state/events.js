862cb7a5252af1d59f99136df48f63b2320a21ce
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-04-10 00:26:08 +0200

    redux state + fetch added

---
+++ src/state/events.js
@@ -0,0 +1,32 @@
+const SET_EVENTS = 'events/SET_EVENTS'
+
+const setEvents = eventsData => ({
+    type: SET_EVENTS,
+    eventsData
+})
+const APIKEY = 'isvxjalFjGmAQCYMDG0mBf9h73x8yJDQ'
+const URL = `https://app.ticketmaster.com/discovery/v2/`
+
+export const getEvents = SearchInput => dispatch => {
+    fetch(`${URL}events.json?countryCode=${SearchInput}&apikey=${APIKEY}`)
+        .then(response => response.json())
+        .then(data => {
+            dispatch(setEvents(data))
+        })
+}
+
+const initialState = {
+    eventsData: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_EVENTS:
+            return {
+                ...state,
+                eventsData: action.eventsData
+            }
+        default:
+            return state
+    }
+}
