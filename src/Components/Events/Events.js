bd5b0ee48c75a98e182c9091c9852e7659b292bd
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-04-16 19:15:19 +0200

    input added

--- src/Components/Events/Events.js
+++ src/Components/Events/Events.js
@@ -1,13 +1,51 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {getEvents} from "../../state/events";
 
 class Events extends React.Component {
+
+    state = {
+        inputValue: ''
+    }
+
+    handleInputChange = event => {
+        this.setState({
+            inputValue: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.getEvents(this.state.inputValue)
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <form
+                onSubmit={this.handleSubmit}>
+                    <input
+                        type='text'
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange}
+                    />
+                    <input
+                        type='submit'
+                        value='search'
+                    />
+                </form>
             </div>
         )
     }
 }
+const mapDispatchToProps = dispatch => ({
+    getEvents: inputValue => dispatch(getEvents(inputValue))
+})
 
-export default Events
+const mapStateToProps = state => ({
+    eventsData: state.events.eventsData
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps)(Events)
