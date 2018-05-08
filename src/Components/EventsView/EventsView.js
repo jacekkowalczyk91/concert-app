d9e02420155b104947fb22e83d6f0f3a2330b715
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-05-08 23:45:22 +0200

    event view

--- src/Components/EventsView/EventsView.js
+++ src/Components/EventsView/EventsView.js
@@ -2,10 +2,19 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 class EventsView extends React.Component {
+
+
     render() {
         return(
             <div>
+                {
+                    this.props.eventsData && this.props.eventsData.map((event, index) => (
+                        <div key={index}>
+                            <p>{event.page}</p>
 
+                        </div>
+                    ))
+                }
             </div>
         )
     }
